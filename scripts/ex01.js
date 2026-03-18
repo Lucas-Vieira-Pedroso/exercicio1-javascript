55e8e76082bfb0bb1c1c60c30d89373c98b217b8
lucas_v_pedroso <lucas_v_pedroso@estudante.sesisenai.org.br>
2026-03-18 12:54:11 -0300

    add - adicionei exercicio 1

---
+++ scripts/ex01.js
@@ -0,0 +1,15 @@
+function somarNumeros() {
+    let soma = 0;
+    let continuar = true;
+    while (continuar) {
+            let numero = parseFloat(prompt("digite um número para somar"));
+            if (!isNaN(numero)) {
+                soma += numero;
+                console.log(soma);
+            } else {
+                alert("por favor, digite um número válido");
+            }
+            continuar = confirm("deseja adicionar mais um número");
+        }
+        alert("A soma dos números é: " + soma);
+}
