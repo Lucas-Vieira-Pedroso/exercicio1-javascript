183c070a6aefd02d9f547c16d84d08613dda8b6d
lucas_v_pedroso <lucas_v_pedroso@estudante.sesisenai.org.br>
2026-03-20 11:36:09 -0300

    add - adicionei ex02

---
+++ scripts/ex02.js
@@ -0,0 +1,10 @@
+function regressiva(){
+    if(confirm("deseja começar contagem")){
+        for(let i=10; i>0;i--){
+            alert(i);
+        }
+        }else{
+            alert("lançamento não realizado");
+        }
+        alert("lançamento realizado");
+}
