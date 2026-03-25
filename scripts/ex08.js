408b6cc5e5c633a66d011bd6220fecced5bd321b
lucas_v_pedroso <lucas_v_pedroso@estudante.sesisenai.org.br>
2026-03-25 11:02:27 -0300

    add - adicionei ex08

---
+++ scripts/ex08.js
@@ -0,0 +1,7 @@
+function rand(){
+    let n1=Math.floor(Math.random() * 100);
+    let n2=Math.floor(Math.random() * 100);
+    alert("a diferênca das duas é " + (n1 - n2));
+    alert("o dobro do primeiro mais o triplo do segundo é " + ((n1 * 2) + (n2 * 3)));
+    alert("a multiplicação é " + (n1 * n2));
+}
