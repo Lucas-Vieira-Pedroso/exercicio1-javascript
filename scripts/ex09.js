72d76f20a4e3a3a8620b141272cc89008d5adb5c
lucas_v_pedroso <lucas_v_pedroso@estudante.sesisenai.org.br>
2026-03-25 11:12:15 -0300

    add - adicionei ex09

---
+++ scripts/ex09.js
@@ -0,0 +1,9 @@
+function ordenacaoo(){
+    let n1=Math.floor(Math.random() * 100);
+    let n2=Math.floor(Math.random() * 100);
+    if(n1 > n2){
+        console.log(n2,n1);
+    }else{
+        console.log(n1,n2);
+    }
+}
